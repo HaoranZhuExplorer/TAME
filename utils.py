6e14a8b267583f7a2671e422cc57dd4e6358c79f
HaoranZhuExplorer <haoranzhux@gmail.com>
2023-09-17 18:30:15 -0400

    fix missing function

--- utils.py
+++ utils.py
@@ -23,6 +23,52 @@ def set_random_seeds(random_seed=0):
     random.seed(random_seed)
 
 
+def prepare_dataloader(num_workers=8,
+                       train_batch_size=128,
+                       eval_batch_size=256):
+
+    train_transform = transforms.Compose([
+        transforms.RandomCrop(32, padding=4),
+        transforms.RandomHorizontalFlip(),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.485, 0.456, 0.406),
+                             std=(0.229, 0.224, 0.225))
+    ])
+
+    test_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=(0.485, 0.456, 0.406),
+                             std=(0.229, 0.224, 0.225))
+    ])
+
+    train_set = torchvision.datasets.CIFAR10(root="data",
+                                             train=True,
+                                             download=True,
+                                             transform=train_transform)
+
+    test_set = torchvision.datasets.CIFAR10(root="data",
+                                            train=False,
+                                            download=True,
+                                            transform=test_transform)
+
+    train_sampler = torch.utils.data.RandomSampler(train_set)
+    test_sampler = torch.utils.data.SequentialSampler(test_set)
+
+    train_loader = torch.utils.data.DataLoader(dataset=train_set,
+                                               batch_size=train_batch_size,
+                                               sampler=train_sampler,
+                                               num_workers=num_workers)
+
+    test_loader = torch.utils.data.DataLoader(dataset=test_set,
+                                              batch_size=eval_batch_size,
+                                              sampler=test_sampler,
+                                              num_workers=num_workers)
+
+    classes = train_set.classes
+
+    return train_loader, test_loader, classes
+
+
 def evaluate_model(model, test_loader, device, criterion=None):
 
     model.eval()
